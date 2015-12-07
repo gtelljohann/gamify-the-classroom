76bc68eb91be9431991ea5d9e7f20db1db41b064
Griffin Telljohann <g.telljohann@gmail.com>
2015-12-07 09:48:08 -0500

    Starting to mock out the front end

--- client/components/app.js
+++ client/components/app.js
@@ -1,7 +1,15 @@
 var React = require('react');
+var TopNav = require('./topNav.component');
 
 module.exports = React.createClass({
   render: function() {
-    return (<h1>hello world</h1>)
+    return (<div id="page-container">
+      <TopNav></TopNav>
+      <div id="page-body">
+        <h1>hello 2world</h1>
+
+      </div>
+      <footer>Thanks for visiting</footer>
+    </div>)
   }
 })
---
+++ client/components/topNav.component.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function() {
+    return (<div id="top-nav">
+        Welcome to whatever navbar
+      </div>)
+  }
+})
